a85657b83217b937c9e802c454637a21596b60b5
heyou <heyou_0423@163.com>
2020-01-25 18:32:35 +0800

    update

--- server-consumer/src/main/java/com/heyou/serverconsumer/ServerConsumerApplication.java
+++ server-consumer/src/main/java/com/heyou/serverconsumer/ServerConsumerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @author heyou
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServerConsumerApplication {
